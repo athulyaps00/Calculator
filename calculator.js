59cb25700cff6380fc35eaf77c5905540c1ca755
athulyaps00 <134292791+athulyaps00@users.noreply.github.com>
2024-04-05 23:25:22 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,25 @@
+// JavaScript source code
+let result=document.getElementById("inputext");
+let calculate=(number)=>{
+    result.value+= number;
+}
+
+let Result=()=> {
+    try
+    {
+        result.value=eval(result.value)
+    }
+    catch (err)
+    {
+        alert("Enter the valid result");
+    }
+}
+function clr()
+{
+    result.value=" ";
+}
+function del()
+{
+    result.value=result.value.slice(0,-1);
+
+}
